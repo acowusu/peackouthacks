de87f619727abca7dd73dd218576f7e4be2623bc
Alexo <alexander.c.owusu@gmail.com>
2021-09-17 14:42:02 +0100

    update config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuesax from "vuesax";
 
+import "vuesax/dist/vuesax.css"; //Vuesax styles
 Vue.config.productionTip = false;
-
+Vue.use(Vuesax, {
+  // options here
+});
 new Vue({
   router,
   render: (h) => h(App),
